93ba00d30b32e91a62b66d140c003fb1adfa9e95
Erwin Gunawan <erwin.dg8ts@gmail.com>
2025-03-13 20:49:59 +0700

    remove message in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   const storeName = process.env.STORE_NAME;
   return (
     <div className="w-full min-h-screen flex justify-center items-center">
-      <p>Hello {storeName}</p>
       <Link href="/order" className="p-2 bg-zinc-100 rounded-md">Start new order</Link>
     </div>
   );
